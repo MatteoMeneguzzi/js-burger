6f6dd2fb48a4cceab170f099c8a3638bf92bd901
MatteoMeneguzzi <meneguzzi.matteo16@gmail.com>
2021-03-28 22:32:24 +0200

    complete

--- js/script.js
+++ js/script.js
@@ -13,22 +13,21 @@ var ingredientList = document.getElementsByClassName("ingredient-checkbox");
 var burgerName = document.getElementById("name");
 var displayPrice = document.getElementById("price");
 var coupon = document.getElementById("coupon");
-var coupons = ["AAA", "BBB", "CCC"];
 var addBtn = document.getElementsByClassName("ingredient-add");
 
 // Attivazione pulsante "Add"
-
 for (var i = 0; i < addBtn.length; i++) {
 	var add = addBtn[i];
+
 	add.addEventListener("click", function () {
-		console.log("Click!");
+		console.log("click");
 		console.log(this);
 		console.log(this.previousElementSibling);
-
 		var thisCheckbox = this.previousElementSibling;
 		thisCheckbox.checked = !thisCheckbox.checked;
 	});
 }
+
 // Calc Price
 
 btn.addEventListener("click", function () {
@@ -47,35 +46,24 @@ btn.addEventListener("click", function () {
 	}
 	// console.log(price);
 
+	// COUPON
+
 	var couponCode = coupon.value;
 
-	// if (coupons.includes(couponCode)) {
-	// 	console.log("Codice corretto");
-	// } else {
-	// 	console.log("Codice non corretto");
-	// }
+	var coupons = ["AAA", "BBB", "CCC"];
 
 	for (var i = 0; i < coupons.length; i++) {
-		if (couponCode === coupons[i]) {
+		if (coupons[i] === couponCode) {
 			price -= price * 0.2;
 		} else {
 		}
 	}
+	// if (coupons.includes(couponCode)) {
+	// 	price -= price * 0.2;
+	// } else {
+	// 	console.log("Codice non corretto");
+	// }
 
 	// PREZZO FINALE
 	displayPrice.innerHTML = price.toFixed(2);
 });
-
-// var coupons = ["AAA", "BBB", "CCC"];
-
-// var randomCoupon = "BBB";
-
-// var found = false;
-
-// for (var i = 0; i < coupons.length; i++) {
-// 	if (coupons[i] === randomCoupon) {
-// 		console.log("Corretto");
-// 	} else {
-// 		console.log("Sbagliato");
-// 	}
-// }
