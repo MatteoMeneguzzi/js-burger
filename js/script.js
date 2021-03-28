ae6c31fbc17dfd569aa175c88ed4934e9d19c6c7
MatteoMeneguzzi <meneguzzi.matteo16@gmail.com>
2021-03-28 15:03:12 +0200

    price

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
+//
+//
+//
+//Create Your Burger
+//
+//
+//
 
+// Referenze
+
+var btn = document.getElementById("button");
+
+var burgerName = document.getElementById("name");
+
+var ingredientList = document.getElementsByClassName("ingredient-checkbox");
+
+var displayPrice = document.getElementById("price");
+
+// Calc Price
+
+btn.addEventListener("click", function () {
+	var name = burgerName.value.trim();
+
+	if (name.length == 0) {
+		alert("errore");
+	} else {
+		// PRICE CALC
+		var price = 50;
+
+		for (var i = 0; i < ingredientList.length; i++) {
+			var ingredient = ingredientList[i];
+			if (ingredient.checked === true) {
+				price += parseInt(ingredient.value);
+			} else {
+			}
+		}
+		console.log(price);
+
+		displayPrice.innerHTML = price.toFixed(2);
+	}
+});
